7135a02e2a4557533f421d61d1b3efa15daac7ad
NaitikDarji <naitikdarji119@gmail.com>
2026-08-05 18:32:17 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle?.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+const links = navLinks?.querySelectorAll('a');
+links?.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
